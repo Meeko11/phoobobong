9fca5d5d8337feedf29d135a2257f5286a874b71
mikoeunhelsubong17 <mikoeunhelsubong17@gmail.com>
2025-11-12 03:33:34 +0000

    Initial commit

--- lib/services/auth_service.dart
+++ lib/services/auth_service.dart
@@ -2,8 +2,11 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/foundation.dart';
 
+import 'firestore_service.dart';
+
 class AuthService with ChangeNotifier {
   final FirebaseAuth _auth = FirebaseAuth.instance;
+  final FirestoreService _firestoreService = FirestoreService();
   User? _user;
 
   AuthService() {
@@ -17,17 +20,36 @@ class AuthService with ChangeNotifier {
 
   bool get isAuthenticated => _user != null;
 
-  Future<UserCredential> signUp({required String email, required String password}) async {
+  Future<UserCredential> signUp({
+    required String email,
+    required String password,
+  }) async {
     try {
-      return await _auth.createUserWithEmailAndPassword(email: email, password: password);
+      final userCredential = await _auth.createUserWithEmailAndPassword(
+        email: email,
+        password: password,
+      );
+      if (userCredential.user != null) {
+        await _firestoreService.createUser(
+          userCredential.user!.uid,
+          email,
+        );
+      }
+      return userCredential;
     } on FirebaseAuthException {
       rethrow;
     }
   }
 
-  Future<UserCredential> signIn({required String email, required String password}) async {
+  Future<UserCredential> signIn({
+    required String email,
+    required String password,
+  }) async {
     try {
-      return await _auth.signInWithEmailAndPassword(email: email, password: password);
+      return await _auth.signInWithEmailAndPassword(
+        email: email,
+        password: password,
+      );
     } on FirebaseAuthException {
       rethrow;
     }
